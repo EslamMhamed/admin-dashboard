608bd90227722497b8a26f7d56336d3d105a890a
Eslam AbdelWahab <mohaeslam22@gmail.com>
2025-12-22 14:46:14 +0200

    Make the Cards dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import DashboradCard from "@/components/dashboard/DashboradCard";
+import { Folder, MessageCircle, Newspaper, User } from "lucide-react";
 
 export default function Home() {
  
   return <>
     <div className="flex flex-col md:flex-row justify-between gap-5 mb-5 ">
-      <DashboradCard />
+      <DashboradCard title="Posts" count={100} icon={<Newspaper className="text-slate-500 " size={72} />} />
+      <DashboradCard title="Categories" count={12} icon={<Folder className="text-slate-500 " size={72} />} />
+      <DashboradCard title="Users" count={750} icon={<User className="text-slate-500 " size={72} />} />
+      <DashboradCard title="Comments" count={1200} icon={<MessageCircle className="text-slate-500 " size={72} />} />
     </div>
   </>
 }
--- components/dashboard/DashboradCard.tsx
+++ components/dashboard/DashboradCard.tsx
@@ -1,16 +1,22 @@
-import { Newspaper } from "lucide-react";
+import { LucideIcon } from "lucide-react";
 import { Card, CardContent } from "../ui/card";
 
-function DashboradCard() {
+type DashboardCardProps = {
+    title : string,
+    count : number,
+    icon: React.ReactElement<LucideIcon>
+}
+
+function DashboradCard({title, count, icon}: DashboardCardProps) {
   return (
     <Card className="bg-slate-100 dark:bg-slate-800 p-4">
       <CardContent>
         <h3 className="text-3xl text-center mb-4 font-bold text-slate-500 dark:text-slate-200">
-          Posts
+          {title}
         </h3>
         <div className="flex gap-5 justify-center items-center">
-            <Newspaper className="text-slate-500 " size={72} />
-            <h3 className="text-5xl font-semibold text-slate-500 dark:text-slate-200">100</h3>
+            {icon}
+            <h3 className="text-5xl font-semibold text-slate-500 dark:text-slate-200">{count}</h3>
         </div>
       </CardContent>
     </Card>
