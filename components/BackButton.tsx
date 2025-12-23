5a63dc7c2d9bac8fc053acf1a7b8f8aba8529315
Eslam AbdelWahab <mohaeslam22@gmail.com>
2025-12-23 12:03:40 +0200

    create BackButton component

---
+++ components/BackButton.tsx
@@ -0,0 +1,17 @@
+import { ArrowLeftCircle } from "lucide-react"
+import Link from "next/link"
+
+type BackButtonProps = {
+    text: string,
+    link: string
+}
+
+function BackButton({link, text}:BackButtonProps) {
+  return (
+    <Link href={link} className="text-gray-500 hover:underline flex items-center gap-1 font-bold mb-5" >
+        <ArrowLeftCircle size={18}  /> {text}
+    </Link>
+  )
+}
+
+export default BackButton
